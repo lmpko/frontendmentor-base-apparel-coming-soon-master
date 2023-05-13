bef9507064b006da5a80360cfcadb9f04ae6f7d9
lmpko <lmpkoo@gmail.com>
2023-05-13 12:04:24 +0200

    html structure for card component added

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import logo from "/images/logo.svg"
+import hero from "/images/hero-mobile.jpg"
 
 const CardComponent = () => {
   return (
-    <div>CardComponent</div>
+    <section>
+        <img src={logo} alt="logo" />
+        <img src={hero} alt="hero" />
+        <div className='ovrview'>
+            <h1>We're coming soon</h1>
+                <p>
+                    Hello fellow shoppers! We're currently building our new fashion store. 
+                    Add your email below to stay up-to-date with announcements and our launch deals.
+                </p>
+        </div>
+        <form>
+            <input type="email" placeholder='Email Address'/>
+        </form>
+    </section>
   )
 }
 
