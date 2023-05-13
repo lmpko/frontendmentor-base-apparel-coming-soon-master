439bddad5fc1da94df77bf482ef5b3c31473d10b
lmpko <lmpkoo@gmail.com>
2023-05-13 12:50:44 +0200

    mobile device css done

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import logo from "/images/logo.svg"
 import hero from "/images/hero-mobile.jpg"
+import arrow from "/images/icon-arrow.svg"
 
 const CardComponent = () => {
   return (
-    <section>
-        <img src={logo} alt="logo" />
-        <img src={hero} alt="hero" />
-        <div className='ovrview'>
-            <h1>We're coming soon</h1>
+    <section className='card'>
+        <img src={logo} alt="logo" className='logo' />
+        <img src={hero} alt="hero" className='hero'/>
+        <div className='overview'>
+            <h1><span>We're</span> coming soon</h1>
                 <p>
                     Hello fellow shoppers! We're currently building our new fashion store. 
                     Add your email below to stay up-to-date with announcements and our launch deals.
                 </p>
         </div>
         <form>
-            <input type="email" placeholder='Email Address'/>
+            <div className='input-container'>
+                <input type="email" placeholder='Email Address'/>
+                <img src={arrow} alt="arrow" className='arrow' />
+            </div>
         </form>
     </section>
   )
